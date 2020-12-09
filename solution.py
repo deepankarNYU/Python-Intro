2a435e4e642d53c5965ea39cb27f1f80ed8f4868
deepankarNYU <60680536+deepankarNYU@users.noreply.github.com>
2020-12-08 19:06:27 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,30 +9,30 @@ def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        print("1")
+        #print("1")
         return "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        print("2")
+        #print("2")
         return "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        print("3")
+        #print("3")
         return "Yes"
     elif question == "Is a hashed message supposed to be un-hashed?":
-        print("4")
+        #print("4")
         return "No"
     elif question ==  "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        print("5")
+        #print("5")
         result = ''
         result += hashlib.md5(b"NYU Computer Networking").hexdigest()
         return result
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        print("6")
+        #print("6")
         return "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        print("7")
+        #print("7")
         return 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        print("8")
+        #print("8")
         return 4
     else:
         return "no"
